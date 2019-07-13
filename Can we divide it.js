61343d5c85473d8e9eb49d3a3630d6c5fb2892c1
Larisa <larisaplantation@gmail.com>
2019-07-12 21:42:20 -0400

    added  file

---
+++ Can we divide it.js
@@ -0,0 +1,7 @@
+/*
+Can we divide it?
+https://www.codewars.com/kata/5a2b703dc5e2845c0900005a/train/javascript
+*/
+function isDivideBy(number, a, b) {
+return (number % a === 0 && number % b === 0) ? true :false;
+}
