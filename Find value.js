755d2d1e42845e21623c877a79103e2058943b52
Larisa <larisaplantation@gmail.com>
2019-07-14 10:43:52 -0400

    added file '*!a == a ?!'

---
+++ Find value.js
@@ -0,0 +1,7 @@
+/*!a == a ?!
+https://www.codewars.com/kata/a-equals-equals-a/train/javascript
+*/
+const a = [];
+console.log(a == false);  //true
+console.log(!a == false); //true
+console.log(a == !a);     //true
