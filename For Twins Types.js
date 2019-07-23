a3cdeba42a62e24cc8034028a4a38515de5a55fd
Larisa <larisaplantation@gmail.com>
2019-07-23 12:50:33 -0400

    added file

---
+++ For Twins Types.js
@@ -0,0 +1,7 @@
+/*
+For Twins: 1. Types
+https://www.codewars.com/kata/for-twins-1-types/train/javascript
+*/
+function typeValidation(variable, type) {
+  return (typeof variable === type)? true:false;
+}
