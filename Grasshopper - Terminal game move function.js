64a85e077d0efe2752acfb80aa2e423e20c631f8
Larisa <larisaplantation@gmail.com>
2019-07-28 20:54:52 -0400

    added file

---
+++ Grasshopper - Terminal game move function.js
@@ -0,0 +1,7 @@
+/*
+Grasshopper - Terminal game move function
+https://www.codewars.com/kata/grasshopper-terminal-game-move-function/train/javascript
+*/
+function move (position, roll) {
+  return position + roll * 2;
+}
