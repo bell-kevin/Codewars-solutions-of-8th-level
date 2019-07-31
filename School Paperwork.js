a5dedf0775ae8710ee9ac100f74573a0d5613ff9
Larisa <larisaplantation@gmail.com>
2019-07-31 16:31:18 -0400

    added file

---
+++ School Paperwork.js
@@ -0,0 +1,8 @@
+/*
+Beginner Series #1 School Paperwork
+https://www.codewars.com/kata/beginner-series-number-1-school-paperwork/train/javascript
+*/
+
+function paperwork(n, m) {
+   return (n < 0 || m <0)? 0: n*m;
+}
