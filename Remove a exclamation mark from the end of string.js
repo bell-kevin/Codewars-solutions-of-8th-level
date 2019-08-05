4925f81c396462b2976e88902e720fc8f4f6b5bf
Larisa <larisaplantation@gmail.com>
2019-08-05 16:26:31 -0400

    added file'remove exclamation mark from the end of string'

---
+++ Remove a exclamation mark from the end of string.js
@@ -0,0 +1,6 @@
+/*
+Exclamation marks series #1: Remove a exclamation mark from the end of string
+https://www.codewars.com/kata/57fae964d80daa229d000126*/
+function remove(s){
+ return s.replace(/\!$/,'');
+}
