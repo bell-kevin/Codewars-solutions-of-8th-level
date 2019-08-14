381891231a9fbf51c2bd55caab3546ac43893092
Larisa <larisaplantation@gmail.com>
2019-08-13 20:49:40 -0400

    added file

---
+++ Geometry Basics Circle Circumference in 2D.js
@@ -0,0 +1,5 @@
+//Geometry Basics: Circle Circumference in 2D
+// https://www.codewars.com/kata/geometry-basics-circle-circumference-in-2d/train/javascript
+function circleCircumference(circle) {
+ return +(2 * Math.PI * circle.radius).toFixed(6);
+}
