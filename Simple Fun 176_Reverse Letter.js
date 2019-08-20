e77b025cd5a97e1fa31be5e94be72008d51e23a7
Larisa <larisaplantation@gmail.com>
2019-08-20 16:37:46 -0400

    added file

---
+++ Simple Fun 176_Reverse Letter.js
@@ -0,0 +1,5 @@
+//Simple Fun #176: Reverse Letter
+//https://www.codewars.com/kata/58b8c94b7df3f116eb00005b
+function reverseLetter(str) {
+  return str.replace(/[^a-zA-Z]/g,'').split('').reverse().join('');
+}
