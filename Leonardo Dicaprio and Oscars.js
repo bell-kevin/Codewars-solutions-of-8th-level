1328e4b883c689dc269581c5c5c4194e3c2de0c5
Larisa <larisaplantation@gmail.com>
2020-01-07 12:02:15 -0500

    added file

---
+++ Leonardo Dicaprio and Oscars.js
@@ -0,0 +1,7 @@
+//Leonardo Dicaprio and Oscars
+//https://www.codewars.com/kata/56d49587df52101de70011e4/
+function leo(oscar){
+  return oscar === 88 ? "Leo finally won the oscar! Leo is happy" :
+            oscar === 86 ? "Not even for Wolf of wallstreet?!" :
+              oscar < 88 ? "When will you give Leo an Oscar?" : "Leo got one already!";
+}
